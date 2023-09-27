9fbd99242bd7a240047ae8df4bed8a1ce59f8999
Tim Ngeno <timngeno424@gmail.com>
2023-09-27 22:53:17 +0300

    modify puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,57 @@
 # Install and configure Nginx web server using puppet
 include stdlib
 
-$link = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+# Update apt repositories
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+}
 
+# Install Nginx package
 package { 'Nginx':
-  ensure => 'installed',
+  ensure  => 'installed',
+  require => Exec['apt-update'],
 }
 
+# Run Nginx service
+service { 'nginx service':
+  ensure    => running,
+  enable    => true,
+  require   => Package['Nginx'],
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
+
+# NGINX CONFIGURATION
+$nginx_configuration = @("EOT")
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    locatioin / {
+      try_files ${uri} ${uri}/ =404;
+    }
+
+    location /redirect_me {
+      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+| EOT
+
+# Default configuration for nginx site
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => $nginx_configuration,
+  require => Package['Nginx'],
+}
+
+
+# Content for html page
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-file_line { 'redirect 301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => "rewrite ^/redirect_me ${link} permanent;",
+  ensure  => file,
+  content => 'Hello World!\n',
+  require => Package['Nginx'],
 }
 
 exec { 'reload configuration':
